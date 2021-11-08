11f37ba1ea887c0ca7a0ed0d322256b838935ea0
zqkang <960623850@qq.com>
2021-11-08 11:30:38 +0800

    Create 04_接口和数组.js

---
+++ Runoob.com/16 接口/04_接口和数组.js
@@ -0,0 +1,4 @@
+var agelist;
+agelist["John"] = 15; // 正确 
+// agelist[2] = "nine"   // 错误,zqkang:编译时即报错：04_接口和数组.ts:14:1 - error TS2322: Type 'string' is not assignable to type 'number'.
+console.log(agelist);
