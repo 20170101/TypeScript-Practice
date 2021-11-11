8ae1be4af07e78e0fd27ad700d8b6766e75d0735
zqkang <960623850@qq.com>
2021-11-11 15:04:45 +0800

    Create 接口.js

---
+++ Runoob.com/16 接口/接口.js
@@ -0,0 +1,18 @@
+var customer = {
+    firstName: 'Tom',
+    lastName: 'Hanks',
+    sayHi: function () { return 'Hi there'; }
+};
+console.log("Customer对象：");
+console.log(customer.firstName);
+console.log(customer.lastName);
+console.log(customer.sayHi());
+var employee = {
+    firstName: 'Jim',
+    lastName: 'Blakes',
+    sayHi: function () { return 'hello'; }
+};
+console.log("Employee 对象：");
+console.log(employee.firstName);
+console.log(employee.lastName);
+console.log(employee.sayHi());
